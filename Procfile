f52428ef34fac9db922e004788d066193e239882
Prince Dalsaniya <prince.dalsaniya@playpowerlabs.com>
2022-02-08 13:46:15 +0530

    Added whitenoise and gunicorn and made it ready for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ppl_training.wsgi --log-file -
--- ppl_training/settings.py
+++ ppl_training/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1lbf_ynyh9+4*t1&_7a-swy*f=70y75mx%gb=a230$^_0xg&iw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ppl_training.herokuapp.com']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,3 +126,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
